4fddf79abfd2eab9b032d1d3adce1967402fc5b5
☙◦ The Tablet ❀ GamerGirlandCo ◦❧ <c.negrissa.the.angel@gmail.com>
2022-08-13 18:57:55 -0400

    Add files via upload

---
+++ src/regexes.ts
@@ -0,0 +1,44 @@
+export const regex = {
+    title_page: /^((title|credit|author[s]?|source|notes|draft date|date|contact|copyright)\:)/gim,
+
+    scene_heading: /^(((int|ext|est|i\/e)[. ]).+)|^(?:\.(?!\.+))(.+)/i,
+    scene_number: /( *#(.+)# *)/,
+
+    transition: /^((FADE (TO BLACK|OUT)|CUT TO BLACK)|.+ TO\:)|^>.+/g,
+
+	character: /^[\^\sA-Z\.]+(\(.+\))*$/gm,
+    dialogue: /^(?:([A-Z*_][0-9A-Z ._\-']*(?:\(.*\))?[ ]*)|\@([A-Za-z*_][0-9A-Za-z (._\-')]*))(\^?)?(?:\n(?!\n+))([\s\S]+)/,
+    parenthetical: /^(\(.+\))$/,
+
+    action: /^(.+)/g,
+    centered: /^(?:> *)(.+)(?: *<)(\n.+)*/g,
+
+    lyrics: /^~(?![ ]).+(?:\n.+)*/,
+
+    section: /^(#+)(?: *)(.*)/,
+    synopsis: /^(?:\=(?!\=+) *)(.*)/,
+
+    note: /^(?:\[{2}(?!\[+))(.+)(?:\]{2}(?!\[+))$/,
+    note_inline: /(?:\[{2}(?!\[+))(.+?)(?:\]{2}(?!\[+))/gs,
+	closing_note: /\]\]$/gm,
+	opening_note: /^\[\[/gm,
+
+    boneyard: /(^\/\*|^\*\/)$/g,
+
+    page_break: /^\={3,}$/,
+    line_break: /^ {2}$/,
+
+    emphasis: /(_|\*{1,3}|_\*{1,3}|\*{1,3}_)(.+)(_|\*{1,3}|_\*{1,3}|\*{1,3}_)/g,
+    bold_italic_underline: /(_{1}\*{3}(?=.+\*{3}_{1})|\*{3}_{1}(?=.+_{1}\*{3}))(.+?)(\*{3}_{1}|_{1}\*{3})/g,
+    bold_underline: /(_{1}\*{2}(?=.+\*{2}_{1})|\*{2}_{1}(?=.+_{1}\*{2}))(.+?)(\*{2}_{1}|_{1}\*{2})/g,
+    italic_underline: /(_{1}\*{1}(?=.+\*{1}_{1})|\*{1}_{1}(?=.+_{1}\*{1}))(.+?)(\*{1}_{1}|_{1}\*{1})/g,
+    bold_italic: /(\*{3}(?=.+\*{3}))(.+?)(\*{3})/g,
+    bold: /(\*{2}(?=.+\*{2}))(.+?)(\*{2})/g,
+    italic: /(\*{1}(?=.+\*{1}))(.+?)(\*{1})/g,
+    underline: /(_{1}(?=.+_{1}))(.+?)(_{1})/g,
+
+    splitter: /\n{2,}/g,
+    cleaner: /^\n+|\n+$/,
+    standardizer: /\r\n|\r/g,
+    whitespacer: /^\t+|^ {3,}/gm
+};
